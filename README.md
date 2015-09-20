5f807ab0d0d94aaa2d261c778bc47fcb2b16582f
Rahul Budhia <rahul.budhia@webaroo.com>
2015-09-21 01:41:30 +0530

    Detailed Readme

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # FlickrApiSample
 Sample application to access Flickr image API using Angular JS
 
-Steps to run the app:
-1. npm install
-2. bower install
-3. grunt serve
+
+Commands to run the app:
+
+
+
+###1. *npm install*
+
+	If Node and NPM is not installed then go to:
+
+	[https://docs.npmjs.com/getting-started/installing-node](https://docs.npmjs.com/getting-started/installing-node)
+
+
+
+###2. *bower install*
+
+	If bower not installed then run:
+
+	*npm install -g bower*
+
+
+
+###3. *grunt serve*
--- app/scripts/services/images.js
+++ app/scripts/services/images.js
@@ -21,12 +21,9 @@ angular.module('flickrappApp')
 					params: {
 						jsoncallback: 'JSON_CALLBACK',
 						method:'flickr.photos.search',
-						//URL suffixed with 't' for 'thumbnai', 'z' for 'medium', similarly 'l' for large
+						//URL suffixed with 't' for 'thumbnail', 'z' for 'medium', similarly 'l' for large
 						extras: 'count_comments,count_faves,description,realname,url_m,url_z',
 						api_key: constants.api.key,
-						// per_page:12,
-						// page:1,
-						// orientation:'square',
 						format:'json'
 					}
 				}
